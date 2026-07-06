e0f5c8aea4600a4fda0e2d6b2d37015d3edd761f
thiagocolen <3965505+thiagocolen@users.noreply.github.com>
2026-07-06 05:53:47 -0300

    Deploy documentation website [closes #1]
    Co-authored-by: open-swe[bot] <open-swe@users.noreply.github.com>

---
+++ assets/js/63c91676.2a16c396.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkdocs=globalThis.webpackChunkdocs||[]).push([[2656],{9100(a){a.exports=JSON.parse('{"metadata":{"permalink":"/patb-cli/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
